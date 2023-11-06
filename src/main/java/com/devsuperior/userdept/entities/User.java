b1d9b35d80498619d05b94c6cfea126ef3c10ec3
RodrigoF32 <rodrigofreitas3223@outlook.com>
2023-11-06 12:45:55 -0300

    Primeiro Spring boot, simples

---
+++ src/main/java/com/devsuperior/userdept/entities/User.java
@@ -0,0 +1,68 @@
+package com.devsuperior.userdept.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+//mapenado a classe para ela representar a tabela indicada até @Gererated
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	
+	//configurando chave estrangeira, ID, pois essa classe recebe um tipo Department para ter essa ligação
+	// É muitos para um, posso ter varios usuarios para um departamento
+	@ManyToOne
+	//Configurando chave estrangeira pelo nome
+	@JoinColumn(name = "department_id")
+	private Department department;
+	
+	public User() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+}
